e116b66fc3d9fe2c094bd5f65ae82c261c8db2ba
Lughx <lughxdr@gmail.com>
2023-01-02 08:39:05 -0600

    third

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 require("dotenv").config()
 const { client } = require("./app")
-const { TOKEN } = process.env
+const { TOKEN, PORT } = process.env
+const http = require("http")
+
+http.createServer((req, res) => {
+    res.write("uwu")
+    res.end()
+}).listen(PORT || 3000)
 
 client.login(TOKEN)
